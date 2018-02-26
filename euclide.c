dd613ba925c38d63d5538fa009bc974c18273cc3
Doriane <doriane.lecam@live.com>
2018-02-26 19:01:13 +0100

    Algo expMod fait. Manque vrai test pour les deux fct

--- euclide.c
+++ euclide.c
@@ -84,20 +84,75 @@ void euclide(mpz_t a, mpz_t p, mpz_t u, mpz_t v) {
         mpz_sub(v,v0,tmp1);
     }
     
-    gmp_printf("q : %Zd, r : %Zd\n",q,r);
+//     gmp_printf("q : %Zd, r : %Zd\n",q,r);
+
+// Libère la mémoire
+    mpz_clear(r0);mpz_clear(r1);
+    mpz_clear(q);mpz_clear(r);
+    mpz_clear(u0);mpz_clear(u1);
+    mpz_clear(v0);mpz_clear(v1);
+    mpz_clear(tmp0);mpz_clear(tmp1);
 }
+
 
 // A = g^a mod p
-void expMod(mpz_t p,mpz_t g,mpz_t a) {
-
+void expMod(mpz_t res,mpz_t p,mpz_t g,mpz_t a) {
+//     printf("Nouvelle boucle\n");
+//     gmp_printf("a = %Zd\n",a);
+//         On applique le modulo après chaque calcul
+    mpz_t mod2,gg,aa,un,deux;
+//     initialisation des variables 
+    mpz_init(gg);
+    mpz_init(aa);
+    mpz_init(deux);
+    mpz_init(un);
+    mpz_init(mod2);
+    mpz_set_d(mod2,2);
+    mpz_set_d(deux,2);    
+    mpz_set_d(un,1);
+//     Si a = 1 le résultat est g
+    if (mpz_cmp_d(a,1)==0) {
+        mpz_mod(gg,g,p);
+        mpz_set(res,gg);
+    }
+//     Si a est pair le résultat est expMod(res,p,g²,a/2)
+    mpz_mod(mod2,a,mod2);
+    if (mpz_cmp_d(mod2,0)==0) {
+//         g²
+        mpz_mul(gg,g,g);
+//         a/2
+        mpz_fdiv_q(aa,a,deux);
+//         Appel récursif
+        expMod(res,p,gg,aa);
+        mpz_mod(res,res,p);
+    } 
+//     Si a > 2 est impair le résultat est g*expMod(res,p,g²,(a-1)/2)
+    else if (mpz_cmp_d(a,2)>0) {
+//         g²
+        mpz_mul(gg,g,g);
+//         a-1
+        mpz_sub(aa,a,un);
+//         (a-1)/2
+        mpz_fdiv_q(aa,aa,deux);
+//         Appel récursif
+        expMod(res,p,gg,aa);
+        mpz_mod(res,res,p);
+//         g*expMod(...)
+        mpz_mul(res,g,res);
+        mpz_mod(res,res,p);
+    }
     
-    
-    
-    
+//     Libère la mémoire
+    mpz_clear(mod2);
+    mpz_clear(gg);
+    mpz_clear(aa);
+    mpz_clear(deux);
+    mpz_clear(un);
 }
 
-
 int main( int argc, char ** argv ) {
+    
+//     Test de la fonction euclide()
     
     mpz_t a,p,u,v;
     mpz_init(a);
@@ -110,7 +165,30 @@ int main( int argc, char ** argv ) {
     
     euclide(a,p,u,v);
     
-    gmp_printf("%Zd*%Zd + %Zd*%Zd = 1\n",a,u,p,v);
-
-
+    gmp_printf("%Zd*(%Zd) + %Zd*(%Zd) = 1\n",a,u,p,v);
+    
+//     Test de la fonction expMod()
+    mpz_t res,pp,gg,aa;
+    mpz_init(res);
+    mpz_init(pp);
+    mpz_init(gg);
+    mpz_init(aa);
+    
+    mpz_set_d(pp,12349);
+    mpz_set_d(gg,2);
+    mpz_set_d(aa,34567);
+    
+    expMod(res,pp,gg,aa);
+    gmp_printf("%Zd^%Zd = %Zd mod %Zd\n",gg,aa,res,pp);
+    
+    
+//     Libère la mémoire
+    mpz_clear(a);
+    mpz_clear(p);
+    mpz_clear(u);
+    mpz_clear(v);
+    mpz_clear(res);
+    mpz_clear(pp);
+    mpz_clear(gg);
+    mpz_clear(aa);
 }
